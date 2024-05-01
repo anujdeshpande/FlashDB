16b3d87a424950c435ac3effba48883bed15886f
Anuj Deshpande <anuj@makerville.io>
2024-05-01 14:59:52 +0530

    Use sfud functions instead of esp_partition functions

--- demos/esp32_ext_spi_flash/components/FlashDB/inc/fal_cfg.h
+++ demos/esp32_ext_spi_flash/components/FlashDB/inc/fal_cfg.h
@@ -15,7 +15,7 @@
 //#define FAL_PART_TABLE_END_OFFSET      65536
 
 /* ===================== Flash device Configuration ========================= */
-extern const struct fal_flash_dev nor_flash0;
+extern struct fal_flash_dev nor_flash0;
 
 /* flash device table */
 #define FAL_FLASH_DEV_TABLE \
@@ -27,8 +27,7 @@ extern const struct fal_flash_dev nor_flash0;
 /* partition table */
 #define FAL_PART_TABLE                                                               \
 {                                                                                    \
-    {FAL_PART_MAGIC_WORD, "fdb_kvdb1", NOR_FLASH_DEV_NAME, 0,         16 * 1024, 0}, \
-    {FAL_PART_MAGIC_WORD, "fdb_tsdb1", NOR_FLASH_DEV_NAME, 16 * 1024, 16 * 1024, 0}, \
+    {FAL_PART_MAGIC_WORD, "fdb_tsdb1", NOR_FLASH_DEV_NAME, 0, 16 * 1024, 0}, \
 }
 #endif /* FAL_PART_HAS_TABLE_CFG */
 
--- demos/esp32_ext_spi_flash/components/FlashDB/porting/fal_flash_esp32_port.c
+++ demos/esp32_ext_spi_flash/components/FlashDB/porting/fal_flash_esp32_port.c
@@ -5,8 +5,7 @@
  */
 
 #include "esp_log.h"
-#include <esp_partition.h>
-
+#include <sfud.h>
 #include <string.h>
 #include <fal.h>
 
@@ -19,7 +18,8 @@
 
 static SemaphoreHandle_t s_lock = NULL;
 
-const static esp_partition_t *partition;
+/* const static esp_partition_t *partition; */
+static sfud_flash* sfud_dev = NULL;
 
 #define LOCK()                                 \
     do                                         \
@@ -46,14 +46,18 @@ static int init(void)
         assert(s_lock != NULL);
     }
 #endif
+    const sfud_flash *flash = sfud_get_device_table() + 0;
+    sfud_dev = flash;
+    if (NULL == sfud_dev)
+    {
+        return -1;
+    }
 
-    // the values passed to esp_partition_find_first() must correspond to the
-    // values set in partitions.csv for the partition named "flashdb".
-    partition = esp_partition_find_first(0x40, 0x00, "flashdb");
+    /* update the flash chip information */
+    nor_flash0.blk_size = sfud_dev->chip.erase_gran;
+    nor_flash0.len = sfud_dev->chip.capacity;
 
-    assert(partition != NULL);
-
-    return 1;
+    return 0;
 }
 
 static int read(long offset, uint8_t *buf, size_t size)
@@ -61,7 +65,7 @@ static int read(long offset, uint8_t *buf, size_t size)
     esp_err_t ret;
 
     LOCK();
-    ret = esp_partition_read(partition, offset, buf, size);
+    ret = sfud_read(sfud_dev,nor_flash0.addr+offset,size, buf);
     UNLOCK();
 
     return ret;
@@ -69,33 +73,37 @@ static int read(long offset, uint8_t *buf, size_t size)
 
 static int write(long offset, const uint8_t *buf, size_t size)
 {
-    esp_err_t ret;
-
     LOCK();
-    ret = esp_partition_write(partition, offset, buf, size);
+    if (sfud_write(sfud_dev, nor_flash0.addr + offset, size, buf) != SFUD_SUCCESS)
+    {
+        return -1;
+    }
+
     UNLOCK();
 
-    return ret;
+    return 0;
 }
 
 static int erase(long offset, size_t size)
 {
-    esp_err_t ret;
     int32_t erase_size = ((size - 1) / FLASH_ERASE_MIN_SIZE) + 1;
 
     LOCK();
-    ret = esp_partition_erase_range(partition, offset, erase_size * FLASH_ERASE_MIN_SIZE);
+    if (sfud_erase(sfud_dev, nor_flash0.addr + offset, size) != SFUD_SUCCESS)
+    {
+        return -1;
+    }   
     UNLOCK();
 
-    return ret;
+    return 0;
 }
 
-const struct fal_flash_dev nor_flash0 =
+struct fal_flash_dev nor_flash0 =
     {
         .name = NOR_FLASH_DEV_NAME,
         .addr = 0x0,                      // address is relative to beginning of partition; 0x0 is start of the partition
-        .len = 32 * 1024,                 // size of the partition as specified in partitions.csv
+        .len = 8*1024*1024,                 // size of the partition as specified in partitions.csv
         .blk_size = FLASH_ERASE_MIN_SIZE, // must be 4096 bytes
         .ops = {init, read, write, erase},
-        .write_gran = 1, // 1 byte write granularity
+        .write_gran = 64, // 1 byte write granularity
 };
