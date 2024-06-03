4493884441c2c08c4a6d326e7cb1b8166e5c19c3
Anuj Deshpande <anuj@makerville.io>
2024-06-03 19:22:55 +0700

    Modify esp32 demo for external flash

--- demos/esp32_spi_flash/components/FlashDB/porting/fal_flash_esp32_port.c
+++ demos/esp32_spi_flash/components/FlashDB/porting/fal_flash_esp32_port.c
@@ -16,9 +16,15 @@
 #ifdef LOCKER_ENABLE
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
-
+#include "esp_flash.h"
+#include "esp_flash_spi_init.h"
+#include "soc/spi_pins.h"
+#include "hal/spi_types.h"
+#include "esp_system.h"
+#include "driver/spi_common.h"
+#include "driver/gpio.h"
 static SemaphoreHandle_t s_lock = NULL;
-
+static char* TAG="fal";
 const static esp_partition_t *partition;
 
 #define LOCK()                                 \
@@ -37,6 +43,54 @@ const static esp_partition_t *partition;
 #define UNLOCK()
 #endif
 
+
+/* static esp_flash_t* example_init_ext_flash(void) */
+static esp_flash_t* init_ext_flash(void)
+{
+	/* These values taken from pins.h */
+    const spi_bus_config_t bus_config = {
+        .mosi_io_num = GPIO_NUM_7,
+        .miso_io_num = GPIO_NUM_4,
+        .sclk_io_num = GPIO_NUM_6,
+    };
+
+    const esp_flash_spi_device_config_t device_config = {
+        .host_id = SPI2_HOST,
+        .cs_io_num = GPIO_NUM_5,
+        /* .io_mode = SPI_FLASH_DIO, */ /* Not sure what this does - commented for now*/
+        .freq_mhz = 10,
+    };
+
+    ESP_LOGI(TAG, "Initializing external SPI Flash");
+    ESP_LOGI(TAG, "Pin assignments:");
+    ESP_LOGI(TAG, "MOSI: %2d   MISO: %2d   SCLK: %2d   CS: %2d",
+        bus_config.mosi_io_num, bus_config.miso_io_num,
+        bus_config.sclk_io_num, device_config.cs_io_num
+    );
+
+    // Initialize the SPI bus
+    ESP_LOGI(TAG, "DMA CHANNEL: %d", SPI_DMA_CH_AUTO);
+    ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &bus_config, SPI_DMA_CH_AUTO));
+
+    // Add device to the SPI bus
+    esp_flash_t* ext_flash;
+    ESP_ERROR_CHECK(spi_bus_add_flash_device(&ext_flash, &device_config));
+
+    // Probe the Flash chip and initialize it
+    esp_err_t err = esp_flash_init(ext_flash);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to initialize external Flash: %s (0x%x)", esp_err_to_name(err), err);
+        return NULL;
+    }
+
+    // Print out the ID and size
+    uint32_t id;
+    ESP_ERROR_CHECK(esp_flash_read_id(ext_flash, &id));
+    ESP_LOGI(TAG, "Initialized external Flash, size=%" PRIu32 " KB, ID=0x%" PRIx32, ext_flash->size / 1024, id);
+
+    return ext_flash;
+}
+
 static int init(void)
 {
 #ifdef LOCKER_ENABLE
@@ -49,8 +103,14 @@ static int init(void)
 
     // the values passed to esp_partition_find_first() must correspond to the
     // values set in partitions.csv for the partition named "flashdb".
-    partition = esp_partition_find_first(0x40, 0x00, "flashdb");
-
+    /* partition = esp_partition_find_first(0x40, 0x00, "flashdb"); */
+    esp_flash_t* flash = init_ext_flash();
+    if (flash == NULL) {
+	    ESP_LOGE(TAG,"Error in ext flash init");
+	    return -1;
+    }
+    const size_t offset = 0;
+    esp_err_t ret = esp_partition_register_external(flash,  offset,  flash->size, "flashdb", 0x40, 0x00, &partition);
     assert(partition != NULL);
 
     return 1;
